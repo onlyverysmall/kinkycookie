043e5132f7ad3695a0e28a035f716cd29568c410
Sarah Hadley <sarah@onlyverysmall.com>
2013-05-06 19:27:45 -0700

    Refactor tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,18 +3,32 @@ require 'spec_helper'
 describe "Static pages" do
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_selector 'title', text: full_title(page_title) }
+  end
+
   describe "Home page" do
     before { visit root_path } 
+    let(:heading) { 'Kinky Cookie' }
+    let(:page_title) { '' }
 
-    it { should have_selector('h1', text: 'Kinky Cookie') }
-    it { should have_selector 'title', text: full_title('') }
+    it_should_behave_like "all static pages" 
     it { should_not have_selector 'title', text: '| Home' }
   end
 
   describe "About page" do
     before { visit about_path } 
-    
-    it { should have_selector('h1',    text: 'About') }
-    it { should have_selector('title', text: full_title('About')) }
+    let(:heading) { 'About' }
+    let(:page_title) { 'About' }
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About')
+    click_link "Home"
+    click_link "kinky cookie"
+    page.should have_selector 'title', text: full_title('')
   end
 end
